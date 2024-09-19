d25700eab8b1823b663aefc4bb27e96826822f0b
Doup22 <dustinsnyder31@gmail.com>
2024-09-19 19:07:33 -0400

    LocationSelectPatch.cs

--- BepInEx/LocationSelectPatch.cs
+++ BepInEx/LocationSelectPatch.cs
@@ -1,31 +1,45 @@
-using Aki.Reflection.Patching;
-using Aki.Reflection.Utils;
-using Aki.Common.Http;
 using EFT;
 using EFT.UI.Matchmaker;
+using HarmonyLib;
+using SPT.Common.Http;
+using SPT.Reflection.Patching;
+using SPT.Reflection.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EntryPointSelector
 {
-  public class LocationSelectPatch : ModulePatch
-  {
-
-    protected override MethodBase GetTargetMethod()
+    public class LocationSelectPatch : ModulePatch
     {
-      var desiredType = typeof(MatchmakerOfflineRaidScreen);
-      var desiredMethod = desiredType.GetMethod("Show", PatchConstants.PrivateFlags);
 
-      return desiredMethod;
+        protected override MethodBase GetTargetMethod()
+        {
+            var desiredType = typeof(MatchmakerOfflineRaidScreen);
+            var desiredMethod = desiredType.GetMethod("Show", PatchConstants.PrivateFlags);
+
+            return desiredMethod;
+        }
+
+        [PatchPrefix]
+        private static bool PatchPrefix(
+          RaidSettings raidSettings
+        )
+        {
+            Logger.LogInfo($"EntryPointSelector {raidSettings?.LocationId}");
+            var jsonPayload = new
+            {
+                locationId = raidSettings?.LocationId
+            };
+
+            RequestHandler.PostJson("/eps/location", "{\"locationId\": \"" + raidSettings?.LocationId + "\"}");
+
+            return true;
+        }
     }
 
-    [PatchPrefix]
-    private static bool PatchPrefix(
-      RaidSettings raidSettings
-    )
-    {
-      Logger.LogInfo($"EntryPointSelector {raidSettings?.LocationId}");
-      RequestHandler.PostJson("/eps/location", "{\"locationId\": \"" + raidSettings?.LocationId + "\"}");
-      return true;
-    }
-  }
+
 }
