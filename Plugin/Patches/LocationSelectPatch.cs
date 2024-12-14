846893d6ffa872b7897ee596398ca491926a2ca0
Doup22 <dustinsnyder31@gmail.com>
2024-12-14 08:27:54 -0500

    Updated desired Method and updated PatchPrefix

--- Plugin/Patches/LocationSelectPatch.cs
+++ Plugin/Patches/LocationSelectPatch.cs
@@ -15,7 +15,7 @@ namespace EntryPointSelector
         {
             // Hook into the public instance method "Show" with parameters (InfoClass, RaidSettings)
             var desiredType = typeof(MatchmakerOfflineRaidScreen);
-            var desiredMethod = desiredType.GetMethod("Show", BindingFlags.Public | BindingFlags.Instance, null, new Type[] { typeof(InfoClass), typeof(RaidSettings) }, null);
+            var desiredMethod = desiredType.GetMethod("Show", BindingFlags.Public | BindingFlags.Instance, null, new Type[] { typeof(MatchmakerOfflineRaidScreen.CreateRaidSettingsForProfileClass) }, null);
 
             if (desiredMethod == null)
             {
@@ -30,23 +30,23 @@ namespace EntryPointSelector
         }
 
         [PatchPrefix]
-        public static bool PatchPrefix(InfoClass profileInfo, RaidSettings raidSettings)
+        public static bool PatchPrefix(MatchmakerOfflineRaidScreen.CreateRaidSettingsForProfileClass controller)
         {
-            if (raidSettings == null)
+            if (controller == null || controller.RaidSettings == null)
             {
-                Logger.LogError("raidSettings is null in PatchPrefix.");
+                Logger.LogError("controller or RaidSettings is null in PatchPrefix.");
                 return true;
             }
 
-            Logger.LogInfo($"EntryPointSelector invoked for LocationId: {raidSettings.LocationId}");
+            Logger.LogInfo($"EntryPointSelector invoked for LocationId: {controller.RaidSettings.LocationId}");
 
             // Create a JSON payload with the selected location ID
-            var payload = "{\"locationId\": \"" + raidSettings.LocationId + "\"}";
+            var payload = "{\"locationId\": \"" + controller.RaidSettings.LocationId + "\"}";
 
             // Send the HTTP POST request
             RequestHandler.PostJson("/eps/location", payload);
 
-            return true;
+            return true; // Allow the original method to execute
         }
     }
 }
